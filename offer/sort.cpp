bc3971f6258b84b2c6c5d5ae4da53d1a0886e53a
aujbl <aujbl.scut@gmail.com>
2022-03-30 17:59:23 +0800

    update

--- offer/sort.cpp
+++ offer/sort.cpp
@@ -78,44 +78,136 @@ public:
     // }
 // ------------------------random quick sort end------------------
     vector<int> quickSort(vector<int>& nums){
-        return _quickSort(nums, 0, nums.size()-1);
-    }
-
-    // 超时去掉返回值
-    vector<int> _quickSort(vector<int>& nums, int l, int r){
-        if (r <= l) return nums;
-        int i = l, j = r + 1;
-
-        // 随机取主元，否则大数组可能超时
-        int pos = ((r - l + 1) >> 1) + l;
-        swap(nums[pos], nums[l]);
-
-        int key = nums[l];
-        while (true){
-            while (nums[++i] < key){
-                if (i == r) break;
-            }
-            while (nums[--j] > key){
-                if (j == l) break;
-            }
-            if (i >= j) break;
-
-            swap(nums[i], nums[j]);
-        }
-        nums[l] = nums[j];      // j对应 <= key的值，与key交换不出错
-        nums[j] = key;          // j位置排序完成
-        _quickSort(nums, l, j-1);
-        _quickSort(nums, j+1, r);
+        _quickSort(nums, 0, nums.size()-1);
         return nums;
     }
 
+    void _quickSort(vector<int>& nums, int l, int r){
+    if (r <= l) return;
+    int pos = ((r - l + 1) >> 1) + l;
+    int key = nums[pos];
+    swap(nums[l], nums[pos]);
+    int i = l, j = r + 1;                   // i = l,因为l位置保存了主元的值       
+    while (true){
+        while (nums[++i] < key){
+            if (i == r) break;
+        }
+        while (nums[--j] > key){
+            if (j == l) break;
+        }
+        if (i >= j) break;
+        swap(nums[i], nums[j]);
+    }
+    swap(nums[l], nums[j]);
+    _quickSort(nums, l, j-1);
+    _quickSort(nums, j+1, r);
+    }
+
+    // // 超时去掉返回值
+    // vector<int> _quickSort(vector<int>& nums, int l, int r){
+    //     if (r <= l) return nums;
+    //     int i = l, j = r + 1;
+
+    //     // 随机取主元，否则大数组可能超时
+    //     int pos = ((r - l + 1) >> 1) + l;
+    //     swap(nums[pos], nums[l]);
+
+    //     int key = nums[l];
+    //     while (true){
+    //         while (nums[++i] < key){
+    //             if (i == r) break;
+    //         }
+    //         while (nums[--j] > key){
+    //             if (j == l) break;
+    //         }
+    //         if (i >= j) break;
+
+    //         swap(nums[i], nums[j]);
+    //     }
+    //     nums[l] = nums[j];      // j对应 <= key的值，与key交换不出错
+    //     nums[j] = key;          // j位置排序完成
+    //     _quickSort(nums, l, j-1);
+    //     _quickSort(nums, j+1, r);
+    //     return nums;
+    // }
+    void mergeSort(vector<int>& nums){
+        _mergeSort(nums, 0, nums.size() - 1);
+    }
+
+    void _mergeSort(vector<int>& nums, int l, int r){
+        if (l >= r) return;
+        int mid = (l + r) >> 1;             // l + ((r - l) >> 1)
+        _mergeSort(nums, l, mid);
+        _mergeSort(nums, mid+1, r);
+        vector<int> tmp(r - l + 1);
+        int i = l, j = mid + 1;
+        int cnt = 0;
+        while (i <= mid && j <= r){
+            if (nums[i] <= nums[j]){
+                tmp[cnt++] = nums[i++];
+            }else{
+                tmp[cnt++] = nums[j++];
+            }
+        }
+        while (i <= mid){
+            tmp[cnt++] = nums[i++];
+        }
+        while (j <= r){
+            tmp[cnt++] = nums[j++];
+        }
+        for (int i=0; i<r-l+1; ++i){
+            nums[i+l] = tmp[i];
+        }
+    }
+
+    void maxHeapify(vector<int>& nums, int l, int r){
+        int dad = l;
+        int son = (dad << 1) + 1;
+        while (son <= r){
+            // 父节点最大，子节点哪个大不确定
+            if (son + 1 <= r && nums[son] < nums[son+1]){
+                ++son;              // 让son指向较大的值
+            }
+            if (nums[dad] > nums[son]){
+                return;
+            }else{
+                swap(nums[dad], nums[son]);
+                dad = son;
+                son = (dad << 1) + 1;
+            }
+        }
+    }
+
+    void _heapSort(vector<int>& nums, int len){
+        // 从最后一个父节点len / 2 - 1开始调整，调整后形成大根堆
+        for (int i=len/2-1; i>=0; --i){
+            maxHeapify(nums, i, len-1);         // 得到一个最大值，保存在nums[0]，i~len-1是调整好的部分
+        }
+        for (int i=len-1; i>0; --i){
+            swap(nums[i], nums[0]);             // 把nums[0]处的最大值移到最后
+            maxHeapify(nums, 0, i-1);           // 向下调整，直到父节点比两个子节点都大
+        }
+    }
+
+    void heapSort(vector<int>& nums){
+        _heapSort(nums, nums.size());
+    }
+
+
+
+
 };
+
+
 
 int main(){
     Solution solution;
-    vector<int> nums = {};
+    // vector<int> nums = {1,4,5,41246,7,7432,9,9,1341233,71341234,6213,4,3,3,5,6,7,8,8,6};
+    vector<int> nums = {5, 2, 3, 1};
     // solution.selectSort(nums);
-    solution.quickSort(nums);
+    // solution.quickSort(nums);
+    // solution.mergeSort(nums);
+    solution.heapSort(nums);
 
 
     for (auto n:nums){
@@ -125,3 +217,7 @@ int main(){
 
     return 0;
 }
+
+
+
+
--- test1.cpp
+++ test1.cpp
@@ -1,21 +1,62 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-class P
-{
-public:
-    P(int xx=0){X = xx;}
-    
-    static void show(P &q){cout << q.X;};
-
+class Solution {
 private:
-    int X;
+    int m, n;
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        vector<int> dirs = {1, 0, -1, 0, 1};
+        vector<vector<int>> visited(m, vector<int> (n, 0));
+        int cnt = 0;
+        for (int i=0; i<m; ++i){
+            for (int j=0; j<n; ++j){
+                if ((grid[i][j] == '1') && (visited[i][j] == 0)){
+                    ++cnt;
+                    dfs(grid, dirs, visited, i, j);
+                }else{
+                    visited[i][j] = 1;
+                }
+            }
+        }
+
+        return cnt;
+    }
+
+    void dfs(vector<vector<char>>& grid, vector<int>& dirs, vector<vector<int>>& visited, int i, int j){
+        queue<vector<int>> q;
+        q.push({i, j});
+        visited[i][j] = 1;
+        while (!q.empty()){
+            vector<int> tmp = q.front();    
+            q.pop();
+            int x = tmp[0], y = tmp[1];
+            for (int k=0; k<4; ++k){
+                int x1 = x + dirs[k], y1 = y + dirs[k+1];
+                if ((x1 >=0 && x1 < m) && (y1 >= 0 && y1 < n) && (grid[x1][y1] == '1') && (visited[x1][y1] == 0)){
+                    visited[x1][y1] = 1;
+                    q.push({x1, y1});
+                }
+            }
+        }
+    }
+
 };
 
 
-int main(){
-    P A(5);
-    P B(A);
-    P::show(B);
+
+
+int main(int argc, char const *argv[])
+{
+    Solution solution;
+
+    vector<vector<char>> grid = {{'1', '2', '3'}, {'4', '5', '6'}};
+
+    solution.numIslands(grid);
+
     return 0;
 }
