23dfd5377c90702f7f8514748cb80293ea9f3c81
aujbl <aujbl.scut@gmail.com>
2022-04-07 17:57:59 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,34 +1,26 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-typedef pair<int, int> mytyp;
-
-struct cmp{
-    bool operator() (const mytyp& t1, const mytyp& t2){
-        return t1.second > t2.second;
-    }
-};
-
-
-int main(int argc, char const *argv[])
-{
-    priority_queue<mytyp, vector<mytyp>, cmp > q;
-
-    mytyp b(1, 2);
-    mytyp c(1, 3);
-    mytyp d(2, 5);
-    mytyp e(2, 2);
-
-    q.emplace(b);
-    q.emplace(c);
-    q.emplace(d);
-    q.emplace(e);
-
-    while (!q.empty()){
-        cout << q.top().first << " " << q.top().second << endl;
-        q.pop();
-    }
-
+int main(){
+    string s;
+    s = "assssa";
+    // while (cin >> s){
+        vector<int> cnt(26, 0);
+        int min_oc = INT_MAX;
+        for (auto ch : s){
+            cnt[ch - 'a']++;
+            min_oc = min(min_oc, cnt[ch - 'a']);
+        }
+        for (auto ch : s){
+            if (cnt[ch - 'a'] == min_oc){
+                continue;
+            }
+            cout << ch;
+        }
+        cout << endl;
+        // s.clear();
+    // }
+        
+    
     return 0;
 }
---
+++ 常用算法/priority_q.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef pair<int, int> mytyp;
+
+// 自定义排序
+struct cmp{
+    bool operator() (const mytyp& t1, const mytyp& t2){
+        return t1.second > t2.second;
+    }
+};
+
+
+int main(int argc, char const *argv[])
+{
+    priority_queue<mytyp, vector<mytyp>, cmp > q;
+
+    mytyp b(1, 2);
+    mytyp c(1, 3);
+    mytyp d(2, 5);
+    mytyp e(2, 2);
+
+    q.emplace(b);
+    q.emplace(c);
+    q.emplace(d);
+    q.emplace(e);
+
+    while (!q.empty()){
+        cout << q.top().first << " " << q.top().second << endl;
+        q.pop();
+    }
+
+    return 0;
+}
